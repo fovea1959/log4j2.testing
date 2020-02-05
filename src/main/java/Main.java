a9868359d508feff6f77fe8391bf96e4eece549a
Doug Wegscheid <dwegscheid@sbcglobal.net>
2020-02-05 09:47:41 -0500

    Works w/ slf4j API.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
-    static Logger logger = LogManager.getRootLogger();
+    static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         // System.setProperty("log4j2.contextSelector", "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
+        logger.warn ("Start");
         for (int i = 0; i < 10; i++) {
             logger.info("hi bob {}", i);
             try {
